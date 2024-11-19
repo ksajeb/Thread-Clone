245876ea09a2b41f8958b285180d3549d0f92de0
SAJEB KHAN <sajebk007@gmail.com>
2024-11-19 17:04:24 +0530

    basic register ui page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/common/Header";
-import Home from "./pages/protected/Home"
-import Search from "./pages/protected/Search"
+import Home from "./pages/protected/Home";
+import Search from "./pages/protected/Search";
 import Error from "./pages/Error";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <>
+      <Register />
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/search" element={<Search/>} />
-          <Route exact path="*" element={<Error/>} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/search" element={<Search />} />
+          <Route exact path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,38 @@
-
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 const Register = () => {
   return (
-    <div>
-      register
+    <div
+      className="w-full h-screen flex flex-row items-center justify-center"
+      style={{
+        backgroundImage: "url('./thread2.avif')",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100% 600px",
+      }}
+    >
+      <div className="flex flex-col w-[400px] gap-2 bg-slate-100 p-10 rounded-lg">
+        <p className="font-semibold text-2xl items-center flex text-black">
+          Register with email
+        </p>
+        <Input type="text" placeholder="Enter Your Name" name="name" />
+        <Input type="email" placeholder="Enter Your Email" name="email" />
+        <Input
+          type="password"
+          placeholder="Enter Your Password"
+          name="password"
+        />
+        <Button className="w-full h-10 bg-green-500 text-white text-lg hover:bg-blue-400 cursor-pointer duration-500 ease-in">
+          Register
+        </Button>
+        <p>
+          Already have an account ?
+          <span className="cursor-pointer hover:text-blue-600 hover:underline ml-1">
+            Login
+          </span>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
